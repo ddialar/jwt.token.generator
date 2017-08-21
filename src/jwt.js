4baf00912c7ec7f9f78ad3232b922fb561ec03a9
ddialar <ddialar@gmail.com>
2017-08-21 10:52:04 +0100

    feat(jwt): Implement the file.

---
+++ src/jwt.js
@@ -0,0 +1,30 @@
+import jwt from 'jwt-simple';
+import moment from 'moment';
+
+const encodeJwt = (userId, expirationPeriod, encryptionKey) => {
+    let expirationPeriodParts = expirationPeriod.split(' ');
+    let payload = {};
+    let token = null;
+
+    payload.sub = userId;
+    payload.iat = moment().unix();
+    payload.exp = moment().add(expirationPeriodParts[0], expirationPeriodParts[1]).unix();
+
+    return jwt.encode(payload, encryptionKey);
+};
+
+const decodeJWT = (token, encryptionKey) => {
+    return jwt.decode(token, encryptionKey);
+};
+
+const checkIfTokenIsUpToDate = (token, encryptionKey) => {
+    let decodedToken = jwt.decode(token, encryptionKey);
+
+    return moment(moment().unix()).isBefore(decodedToken.exp);
+};
+
+export {
+    encodeJwt,
+    decodeJWT,
+    checkIfTokenIsUpToDate
+};
